06a4121d6974bc1fbe36f440288e56cb786558c7
Daniel Callander <knifacat@gmail.com>
2017-10-28 10:17:38 +0100

    Fix double websocket connection

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,8 +3,6 @@ import * as io from 'socket.io-client';
 
 import DeviceBox from '../DeviceBox/DeviceBox';
 
-const socket = io.connect('http://127.0.0.1:5000/');
-
 export default () =>
   <div>
     <DeviceBox />
