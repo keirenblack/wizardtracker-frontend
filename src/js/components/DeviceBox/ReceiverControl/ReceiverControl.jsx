f4185dfe1de7d58ca6277f1a8d07d3e73facd35c
Daniel Callander <knifacat@gmail.com>
2017-11-02 03:34:22 +0000

    Show selectable channels in receiver controller

--- src/js/components/DeviceBox/ReceiverControl/ReceiverControl.jsx
+++ src/js/components/DeviceBox/ReceiverControl/ReceiverControl.jsx
@@ -1,16 +1,45 @@
 import React from 'react'
-import styles from './ReceiverControl.scss';
+import styles from './ReceiverControl.scss'
 
+import channels from 'utils/channels'
 
-export default function (props) {
-  const rssiPercent = Math.round(props.rssi / 255 * 100) + '%';
+export default function ReceiverControl (props) {
+  const rssiPercent = Math.round(props.rssi / 255 * 100) + '%'
   const innerStyle = {
     width: rssiPercent
   }
 
+  const frequencyOptions = []
+  for (const i in channels.all) {
+    const channel = channels.all[i]
+
+    frequencyOptions.push(
+      <option
+        key={i}
+        value={channel.frequency}
+      >
+        {channel.name} - {channel.frequency}
+      </option>
+    )
+  }
+
+  const selectedFrequency = channels.findByFrequency(props.frequency)
+
   return <div className={styles.ReceiverControl}>
-    <select className={styles.FrequencySelector}>
-      <option>{props.frequency}</option>
+    <select
+      className={styles.FrequencySelector}
+      value={selectedFrequency && selectedFrequency.frequency || 'unknown'}
+      readOnly={true}
+    >
+      <option
+        value={'unknown'}
+        disabled
+        hidden={selectedFrequency !== undefined}
+      >
+        ?? - {props.frequency}
+      </option>
+
+      {frequencyOptions}
     </select>
 
     <div className={styles.RssiBar}>
